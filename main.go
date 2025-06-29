988f51b15fdeca6d8324146bd5a9f6ab28ee193a
VMT1312 <thiemmavinh123@gmail.com>
2025-06-29 15:12:08 +0800

    Move the body of the function into repl.go instead

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-
-		if !scanner.Scan() {
-			fmt.Println("Error reading input, terminating.")
-			break
-		}
-
-		text := scanner.Text()
-		words := cleanInput(text)
-
-		fmt.Printf("Your command was: %s\n", words[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,29 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			fmt.Println("Error reading input, terminating.")
+			break
+		}
+
+		text := scanner.Text()
+		words := cleanInput(text)
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	if text == "" {
