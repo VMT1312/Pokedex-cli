40b4621e072cd6863d27f86fafeadbcb2fce743a
VMT1312 <thiemmavinh123@gmail.com>
2025-06-29 15:09:12 +0800

    Implement main loop for user input in Pokedex

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			fmt.Println("Error reading input, terminating.")
+			break
+		}
+
+		text := scanner.Text()
+		words := cleanInput(text)
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
